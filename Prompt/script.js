707ed787920b5f356ea9ecb9191e56ee10ef64f4
NamraJilani <155049117+NamraJilani@users.noreply.github.com>
2024-04-15 18:15:33 +0500

    Add files via upload

---
+++ Prompt/script.js
@@ -0,0 +1,10 @@
+let first = prompt("What is your first name")
+console.log(first)
+
+let middle = prompt("What is your middle name")
+console.log(middle)
+
+
+
+let last = prompt("What is your last name")
+console.log(last)
